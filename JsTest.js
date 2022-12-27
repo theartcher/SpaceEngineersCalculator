dc96d361d2a0a1f8becb0c8012c2e51f4dcc1d7e
TheArtcher <jorisplayz@gmail.com>
2022-12-27 23:53:13 +0100

    Fixed bug that json was string, now is correct object

--- JsTest.js
+++ JsTest.js
@@ -1,27 +1,29 @@
-const fs = require('fs');
-var convert = require('xml-js');
+const fs = require("fs");
+var convert = require("xml-js");
 
-const results = fs.readFileSync('./bp.sbc', 'utf8', (error, contents) => {
-    if (error) {
-        console.error(error);
-    }
-    else {
-        console.log("Ran through function.");
-    }
-    return contents
+const results = fs.readFileSync("./bp.sbc", "utf8", (error, contents) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log("Ran through function.");
+  }
+  return contents;
 });
 
+var ResultsJsonString = convert.xml2json(results, { compact: true, spaces: 4 });
+var ResultsJsonObject = JSON.parse(ResultsJsonString);
 
+console.log(typeof results);
+console.log(typeof ResultsJsonObject);
 
-var resultsJSON = convert.xml2json(results, {compact: true, spaces: 4});
-// console.log(results)
-// console.log(resultsJSON)
-
-fs.writeFileSync("Response.json", resultsJSON, (error) => {
+fs.writeFileSync(
+  "Response.json",
+  JSON.stringify(ResultsJsonObject),
+  (error) => {
     if (error) {
-        console.error(error);
+      console.error(error);
+    } else {
+      console.log("File written successfully.");
     }
-    else {
-        console.log("File written successfully.");
-    }
-})
+  }
+);
